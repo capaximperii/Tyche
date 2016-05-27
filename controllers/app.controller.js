1ca0aa9624d882be1b6590118af8692595cfa8eb
luv <luv@henoida.com>
2016-05-27 18:34:19 +0530

    Adds better information

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -7,16 +7,49 @@ app.controller('myCtrl', function($scope, $timeout) {
   $scope.events = [{
     badgeClass: 'info',
     badgeIconClass: 'glyphicon-check',
-    title: 'First role',
-    company: 'Some awesome company.',
-    place: 'city name'
+    title: 'Internship',
+    company: 'Henoida',
+    place: 'Paris',
+    year: '2016'
   }, {
     badgeClass: 'warning',
     badgeIconClass: 'glyphicon-credit-card',
-    title: 'Second role',
-    company: 'Some awesome company.',
-    place: 'city name'
-  }];
+    title: 'Masters',
+    company: 'EPITA',
+    place: 'Paris',
+    year: '2014'
+  },
+  {
+    badgeClass: 'info',
+    badgeIconClass: 'glyphicon-credit-card',
+    title: 'Staff Engineer',
+    company: 'Dell Wyse',
+    place: 'Bangalore',
+    year: '2012'
+  },
+  {
+    badgeClass: 'info',
+    badgeIconClass: 'glyphicon-credit-card',
+    title: 'Consultant Engineer',
+    company: 'Qualcomm',
+    place: 'Hyderabad',
+    year: '2010'
+  },{
+    badgeClass: 'warning',
+    badgeIconClass: 'glyphicon-credit-card',
+    title: 'Senior Engineer',
+    company: 'TeamF1 (D-Link)',
+    place: 'Hyderabad',
+    year: '2006'
+  },{
+    badgeClass: 'warning',
+    badgeIconClass: 'glyphicon-credit-card',
+    title: 'Staff Engineer',
+    company: 'Comptrix Systems Pvt Ltd',
+    place: 'Pune',
+    year: '2006'
+  }
+  ];
   
   $scope.pageImage = function() {
     html2canvas(document.body, {  
@@ -30,13 +63,24 @@ app.controller('myCtrl', function($scope, $timeout) {
     });
   };
 
+  $scope.captureAndDisplay = function() {
+    $('body').html2canvas({
+      onrendered: function (canvas) {
+        var myImage = canvas.toDataURL("image/png");
+        window.open(myImage);
+      }
+    });
+  }
+
+
+
   $scope.onLoad = function () {
     $timeout(function() {
-      var h = $("#heightmarker").offset();
+      var h1 = $("#heightmarker1").offset();
+      var h2 = $("#heightmarker2").offset();
       var div =  $('#fullpage');
-      var correction = h.top - div.offset().top;
+      var correction = Math.max(h1.top, h2.top) - div.offset().top;
       div.height(correction);
-      console.log(correction);
     }, 1000);
   }
 
