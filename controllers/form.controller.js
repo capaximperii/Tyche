500f2571100ac9fca1bbd9ae113ac80c4f1cb9d1
luv <luv@henoida.com>
2016-05-28 22:23:46 +0530

    Implements resume builder v1

--- controllers/form.controller.js
+++ controllers/form.controller.js
@@ -9,12 +9,16 @@ app.controller('formCtrl', function($scope, storage, $state) {
 	$scope.addSkill = function() {
 		$scope.cv.skills.push({skill: $scope.skill, proficiency: $scope.proficiency});
 	};
-
 	$scope.addExtraSkill = function() {
 		$scope.cv.extraskills.push($scope.extraskill);
 	};
 	$scope.addTimeline = function() {
-		$scope.cv.timeline.push({year: $scope.year, place: $scope.place, title: $scope.title, company: $scope.company});
+		if($scope.isSchool == true) badgeClass = 'warning';
+		else badgeClass = 'info';
+		$scope.cv.timeline.push({year: $scope.year, place: $scope.place, title: $scope.title, company: $scope.company, badgeClass: badgeClass});
+	};
+	$scope.addReference = function() {
+		$scope.cv.references.push({name: $scope.refname, company: $scope.refcompany, position: $scope.refposition, email: $scope.refemail});
 	};
 
 
